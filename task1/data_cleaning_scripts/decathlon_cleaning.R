275ff5bccb0ff2793123f716d21e82017e5df9d1
vpetrova13 <vpetrova1364@gmail.com>
2020-11-12 11:59:01 +0000

    4day update

--- task1/data_cleaning_scripts/decathlon_cleaning.R
+++ task1/data_cleaning_scripts/decathlon_cleaning.R
@@ -5,5 +5,22 @@ library(janitor)
 decathlon_raw <- read_rds("~/dirty_data_project/task1/raw_data/decathlon.rds") %>% 
   clean_names()
 
+# Remove row names by creating one more column
 decathlon_data <- decathlon_raw %>% 
   rownames_to_column
+
+# Change column names and some rownames
+decathlon_data <- decathlon_data %>% 
+  rename("run_100m" = "x100m",
+         "run_400m" = "x400m",
+         "hurdle_110m" = "x110m_hurdle",
+         "run_1500m" = "x1500m",
+         "names" = "rowname")
+
+decathlon_data <- decathlon_data %>% 
+  mutate(competition = str_to_lower(competition))
+
+#Write csv file
+
+write_csv(decathlon_data, "decathlon_clean_data.csv")
+  
---
+++ task3/data_cleaning_scripts/seabird_cleaning_data.R
@@ -0,0 +1,42 @@
+#Load libraries 
+library(tidyverse)
+library(janitor)
+library(readxl)
+
+#Explore raw data and sheets
+excel_sheets(path = "raw_data/seabirds.xls")
+length(excel_sheets(path = "raw_data/seabirds.xls"))
+
+#Read data
+seabird_raw_ship <- read_xls(path = "raw_data/seabirds.xls", 
+                        sheet = "Ship data by record ID") %>% clean_names()
+seabird_raw_bird <- read_xls(path = "raw_data/seabirds.xls", 
+                        sheet = "Bird data by record ID") %>% clean_names()
+
+#Remove unneeded variables (columns) by analysis another sheets from excel file
+names(seabird_raw_bird)
+bird_codes <- read_xls(path = "raw_data/seabirds.xls", 
+                             sheet = "Bird data codes") %>% clean_names()
+ship_codes <- read_xls(path = "raw_data/seabirds.xls", 
+                       sheet = "Ship data codes") %>% clean_names()
+
+seabird_data <- seabird_raw_bird %>% 
+  select(record:age, sex, count)
+
+ship_data <- seabird_raw_ship %>% 
+  select(record:long, obs, csmeth)
+
+# Join data
+seabird_full_data <- left_join(seabird_data, ship_data, by = "record_id")
+
+seabird_full_data <- seabird_full_data %>% 
+  select(-sex, -date, -time, -obs, -csmeth, -age)
+
+#Write csv file
+
+write_csv(seabird_full_data, "seabird_clean_data.csv")
+
+
+
+
+
